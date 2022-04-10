79cfa45540d4bc646e0fd50d3c0d0430773abcba
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-10 17:40:29 +0200

    07.67 Field Injection

--- src/main/java/com/udemy/spring/TennisCoach.java
+++ src/main/java/com/udemy/spring/TennisCoach.java
@@ -6,26 +6,23 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach{
 
+    @Autowired
     private FortuneService fortuneService;
 
-    @Autowired
-    public void setFortuneServiceMethodRandomName(FortuneService fortuneService){
-        this.fortuneService=fortuneService;
-    }
+//    @Autowired
+//    public void setFortuneServiceMethodRandomName(FortuneService fortuneService){
+//        this.fortuneService=fortuneService;
+//    }
 
-/*
-    @Autowired
-    public void setFortuneService(FortuneService fortuneService){
-        this.fortuneService=fortuneService;
-    }
-    */
-/*
+//    @Autowired
+//    public void setFortuneService(FortuneService fortuneService){
+//        this.fortuneService=fortuneService;
+//    }
 
-    @Autowired
-    public TennisCoach(FortuneService fortuneService) {
-        this.fortuneService = fortuneService;
-    }
-*/
+//    @Autowired
+//    public TennisCoach(FortuneService fortuneService) {
+//        this.fortuneService = fortuneService;
+//    }
 
     @Override
     public String getDailyWorkout() {
