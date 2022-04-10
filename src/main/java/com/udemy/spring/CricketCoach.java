6da3ee4a98c94c35c02a7ccd64c9143b7238fa94
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-10 11:13:19 +0200

    5.35 Setter Injection - Part 1

---
+++ src/main/java/com/udemy/spring/CricketCoach.java
@@ -0,0 +1,20 @@
+package com.udemy.spring;
+
+public class CricketCoach implements Coach{
+
+    private FortuneService fortuneService;
+
+    public void setFortuneService(FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+}
