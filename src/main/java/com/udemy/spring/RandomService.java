ab3be6b69a79b723028202064aaf196c8440ffc3
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-10 17:56:27 +0200

    07.70 Qualifiers for Dependency Injection - Part 1

---
+++ src/main/java/com/udemy/spring/RandomService.java
@@ -0,0 +1,12 @@
+package com.udemy.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomService implements FortuneService{
+
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
