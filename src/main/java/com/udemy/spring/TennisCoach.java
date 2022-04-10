723ec541b199d2a1d8c3c874ba683578879cfb9e
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-10 17:30:30 +0200

    07.64 Setter Injection

--- src/main/java/com/udemy/spring/TennisCoach.java
+++ src/main/java/com/udemy/spring/TennisCoach.java
@@ -9,9 +9,14 @@ public class TennisCoach implements Coach{
     private FortuneService fortuneService;
 
     @Autowired
+    public void setFortuneService(FortuneService fortuneService){
+        this.fortuneService=fortuneService;
+    }
+
+    /*    @Autowired
     public TennisCoach(FortuneService fortuneService) {
         this.fortuneService = fortuneService;
-    }
+    }*/
 
     @Override
     public String getDailyWorkout() {
