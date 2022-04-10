96fa4247014c3e8f26911193f2489ad50e14f3de
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-10 12:39:40 +0200

    6.48 Bean Lifecycle

---
+++ src/main/java/com/udemy/spring/BeanLifeCycleDemoApp.java
@@ -0,0 +1,23 @@
+package com.udemy.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemoApp {
+
+    public static void main(String[] args) {
+
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+
+        // retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        // call new methods to het literal values
+        System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyFortune());
+
+        // close the context
+        context.close();
+    }
+
+}
