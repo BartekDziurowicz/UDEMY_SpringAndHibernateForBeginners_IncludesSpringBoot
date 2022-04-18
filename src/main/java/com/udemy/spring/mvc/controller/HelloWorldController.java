9c018a6273580cb80833d9599ecc6b2b5905179b
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-18 11:41:12 +0200

    12.121 Binding Request Params

--- src/main/java/com/udemy/spring/mvc/controller/HelloWorldController.java
+++ src/main/java/com/udemy/spring/mvc/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.udemy.spring.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -31,6 +32,19 @@ public class HelloWorldController {
         theName = theName.toUpperCase();
         // create the message
         String result = "Yo! " + theName;
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+        // create the message
+        String result = "Hey My Friend " + theName;
         // add message to the model
         model.addAttribute("message", result);
 
