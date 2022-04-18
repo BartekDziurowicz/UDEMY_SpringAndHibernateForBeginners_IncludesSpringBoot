987770bae58d7c4928fff7d71b631d5cb131ff24
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-18 12:55:10 +0200

    14.127-130 Text Fields

---
+++ src/main/java/com/udemy/spring/mvc/utils/Student.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.mvc.utils;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student {
+
+    private String firstName;
+    private String lastName;
+
+}
