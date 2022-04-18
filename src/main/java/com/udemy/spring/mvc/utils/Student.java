873ea6e85cc295eccf6e6cff16fccfcc90802c6c
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-18 13:36:20 +0200

    14.131-133 Drop-Down Lists

--- src/main/java/com/udemy/spring/mvc/utils/Student.java
+++ src/main/java/com/udemy/spring/mvc/utils/Student.java
@@ -1,7 +1,10 @@
 package com.udemy.spring.mvc.utils;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.LinkedHashMap;
 
 @Getter
 @Setter
@@ -9,5 +12,17 @@ public class Student {
 
     private String firstName;
     private String lastName;
+    private String country;
+
+    @Setter(value = AccessLevel.NONE)
+    private LinkedHashMap<String, String> countryOptions;
+
+    public Student() {
+        countryOptions = new LinkedHashMap<>();
+        countryOptions.put("BR", "Brazil");
+        countryOptions.put("FR", "France");
+        countryOptions.put("DE", "Germany");
+        countryOptions.put("IN", "India");
+    }
 
 }
