833e41d5709578144577f6b1e1f43bf20c777db9
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-19 20:13:24 +0200

    19.173-179 Setting Up Hibernate Development Environment

---
+++ src/main/java/com/udemy/spring/jdbc/TestJdbc.java
@@ -0,0 +1,23 @@
+package com.udemy.spring.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:mysql://192.168.0.2:3306/hb_student_tracker";
+        String user = "hbstudent";
+        String pass = "";
+
+        try {
+            System.out.println("Connecting to database...");
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+            System.out.println("Connected!");
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+
+    }
+
+}
