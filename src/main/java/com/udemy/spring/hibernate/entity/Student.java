3fe0da3970957119b026e0101c242345807801ad
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-20 21:06:04 +0200

    20.185 Hibernate Annotations

---
+++ src/main/java/com/udemy/spring/hibernate/entity/Student.java
@@ -0,0 +1,34 @@
+package com.udemy.spring.hibernate.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name="student")
+public class Student {
+
+    @Id
+    @Column(name="id")
+    private int id;
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    @Column(name="email")
+    private String email;
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
