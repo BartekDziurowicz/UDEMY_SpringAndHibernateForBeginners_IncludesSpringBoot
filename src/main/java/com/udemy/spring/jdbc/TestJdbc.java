eabb537cbba924a5373161a1855e1782c2008d0d
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-21 16:51:32 +0200

    21.188-189 Creating and Saving Java Objects + pom file updated, hibernate conf changed

--- src/main/java/com/udemy/spring/jdbc/TestJdbc.java
+++ src/main/java/com/udemy/spring/jdbc/TestJdbc.java
@@ -8,7 +8,7 @@ public class TestJdbc {
     public static void main(String[] args) {
         String jdbcUrl = "jdbc:mysql://192.168.0.2:3306/hb_student_tracker";
         String user = "hbstudent";
-        String pass = "";
+        String pass = "studenthb";
 
         try {
             System.out.println("Connecting to database...");
