dc3495936d2da2043f032cc7b10dd838cb8662e8
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-24 18:43:30 +0200

    26.245-247 @0neToMany - uni-Directional - Create Review Class, Configure Fetch Type

--- src/main/java/com/udemy/spring/hibernate/entity/Course.java
+++ src/main/java/com/udemy/spring/hibernate/entity/Course.java
@@ -3,6 +3,9 @@ package com.udemy.spring.hibernate.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name="course")
 @NoArgsConstructor
@@ -25,5 +28,16 @@ public class Course {
     @JoinColumn(name="instructor_id")
     @ToString.Exclude
     private Instructor instructor;
+
+    @OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+    @JoinColumn(name="course_id")
+    private List<Review> reviews;
+
+    public void add(Review tempReview) {
+        if (reviews == null) {
+            reviews = new ArrayList<>();
+        }
+        reviews.add(tempReview);
+    }
 
 }
---
+++ src/main/java/com/udemy/spring/hibernate/entity/Review.java
@@ -0,0 +1,24 @@
+package com.udemy.spring.hibernate.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="review")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="comment")
+    @NonNull
+    private String comment;
+
+}
