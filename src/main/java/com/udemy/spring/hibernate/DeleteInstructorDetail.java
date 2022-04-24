7b286387534ee94466dedd4455bbc1f8dca18ad5
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2022-04-24 09:41:16 +0200

    21.221 @OneToOne - Bi-Directional - Cascade Delete

---
+++ src/main/java/com/udemy/spring/hibernate/DeleteInstructorDetail.java
@@ -0,0 +1,54 @@
+package com.udemy.spring.hibernate;
+
+import com.udemy.spring.hibernate.entity.Instructor;
+import com.udemy.spring.hibernate.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteInstructorDetail {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // begin
+            session.beginTransaction();
+
+            // get the instructor detail object by "id"
+            int id = 3;
+            InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, id);
+
+            // print the instructor detail
+            System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+
+            // print the assicaiated instructor
+            System.out.println("associated instructor: " + tempInstructorDetail.getInstructor());
+
+            // delete the instructor detail
+            System.out.println("Delete tempInstructorDetail: " + tempInstructorDetail);
+            session.delete(tempInstructorDetail);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        } finally {
+            // handle conenction leak issue
+            session.close();
+
+            factory.close();
+        }
+
+    }
+
+}
